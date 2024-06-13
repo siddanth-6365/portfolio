6d6460fd7b98a61dda50127411fd7585f0a987f1
siddanth reddy <siddanthreddy@siddanths-MacBook-Air.local>
2024-06-13 22:36:17 +0530

    minor fix

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -2,16 +2,15 @@
 import React from 'react'
 import Form from '@/components/Form'
 import Terminal from '@/components/Terminal'
-import { TerminalContextProvider } from "react-terminal";
 
 
 const ContactForm = () => {
     return (
         <div className='flex flex-col md:flex-row'>
             <div className='flex items-center justify-center w-full md:w-1/2'>
-                <TerminalContextProvider>
+               
                     <Terminal />
-                </TerminalContextProvider>
+             
             </div>
             <div className='flex items-center  w-screen md:w-1/2'>
                 <Form />
