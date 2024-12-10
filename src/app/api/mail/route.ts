502d27cd5c21e7ea462c2fbd5e53922e0b0c8cf6
siddanth-6365 <114846048+siddanth-6365@users.noreply.github.com>
2024-12-10 19:23:57 +0530

    Update route.ts

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -1,24 +1,31 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
-import config from "./config.json";
 
 export async function POST(request: Request) {
   const formData = await request.json();
-  console.log(formData);
+
+  const config = {
+    gmail: {
+      user: process.env.GMAIL_USER,
+      password: process.env.GMAIL_PASSWORD,
+      toEmail: process.env.TO_EMAIL,
+    },
+  };
+  console.log("config", config);
 
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",      
-    port: 465,                    
-    secure: true,                 
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
-      user: config.gmail.user,   
+      user: config.gmail.user,
       pass: config.gmail.password,
     },
   });
 
   const mailOptions = {
     from: config.gmail.user,
-    to: "siddanth6365@gmail.com",
+    to: config.gmail.toEmail,
     subject: formData.subject,
     text: `Name: ${formData.name}\nEmail: ${formData.email}\n\n body: ${formData.body}`,
   };
